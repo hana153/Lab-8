156ca17326e1cef4ef960fdb4771eab5e4b8683f
Hana <hanabananak@gmail.com>
2019-03-25 22:32:49 -0500

    Added ToDo file to src

---
+++ src/Todo.java
@@ -0,0 +1,30 @@
+public class Todo {/*
+Lab Section_________: 12
+Last Modified_______: 2019-03-11
+OU ID_______________: 113327542
+OU EMAIL____________: hana.k.stevenson-1@ou.edu
+
+Tasks:
+1. Create Sick Person class
+2. Create Healthy Person class
+3. Create Stack Hospital Class
+4. Create Queue Hospital Class
+5. Create Priority Queue Hospital Class
+6. Create test cases
+
+Estimated Time (minutes):
+1. 15
+2. 15
+3. 20
+4. 20
+5. 20
+6. 75
+
+Actual Time (minutes):
+1. 10
+2. 10
+3. 15
+4. 15
+5. 15
+6. 70
+*/}
